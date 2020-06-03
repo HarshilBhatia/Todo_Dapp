9e416f4ebc7b91f2cb1e08cf735b107a0352480b
Harshil Bhatia <harshilbhatia2001@gmail.com>
2020-06-03 19:54:51 +0530

    2.Listing Tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,5 +4,22 @@ pragma solidity ^0.5.0;
 contract TodoList{
     uint public taskCount=0;
 
-    
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+
+    mapping (uint=> Task) public tasks;
+
+    constructor() public{
+        createTasks("Temporary Task");
+    }
+    // function to create new Tasks
+    function createTasks(string memory _content) public {
+        taskCount += 1;
+        tasks[taskCount] = Task(taskCount,_content,false);
+    }
+
 }
